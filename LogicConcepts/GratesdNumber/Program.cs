685f90cfc7eb7ffc6ec604b5d3b86c58c625f31c
Camilo Ibanez <you@example.com>
2025-08-26 22:36:46 -0500

    Complete exercise 1 and 2

---
+++ LogicConcepts/GratesdNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+var response = string.Empty;
+do
+{
+    try
+    {
+        var number1 = ConsoleExtensions.GetInt("Ingrese primer numero: ");
+        var number2 = ConsoleExtensions.GetInt("Ingrese segundo numero: ");
+        var number3 = ConsoleExtensions.GetInt("Ingrese tercer numero: ");
+
+        if (number1 > number2 && number1 > number3)
+        {
+            Console.WriteLine($"El numero mayor es: {number1}");
+        }
+        else if (number2 > number1 && number2 > number3)
+        {
+            Console.WriteLine($"El numero mayor es: {number2}");
+        }
+        else if ( number3 > number1 && number3 > number2)
+        {
+            Console.WriteLine($"El numero mayor es: {number3}");
+        }
+        else
+        {
+            Console.WriteLine("Los numeros son iguales o no se pudo determinar un unico mayot");
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.Write("Desea continuar[S/N]? ");
+    response = Console.ReadLine()!.ToUpper();
+} while (response == "S");
---
+++ LogicConcepts/Shared/ConsoleExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Shared
+{
+    public class ConsoleExtensions
+    {
+
+        public static int GetInt(string message)
+        {
+
+            Console.Write(message);
+            var imput = Console.ReadLine();
+            if (int.TryParse(imput, out int result))
+            {
+                return result;
+            }
+
+            throw new Exception("El valor ingresado no es un numero entero valido");
+
+
+        }
+
+    }
+    
+
+ }
+
